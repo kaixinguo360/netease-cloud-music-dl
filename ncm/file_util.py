49847a2c1854f6933f349126851ca5d2a7e9f428
Kaixinguo <kaixinguo@kaixinguo.site>
2024-01-14 21:22:22 +0800

    Fix the bug of Image.ANTIALIAS

--- ncm/file_util.py
+++ ncm/file_util.py
@@ -13,7 +13,7 @@ def resize_img(file_path, max_size=(640, 640), quality=90):
         return
 
     if img.size[0] > max_size[0] or img.size[1] > max_size[1]:
-        img.thumbnail(max_size, Image.ANTIALIAS)
+        img.thumbnail(max_size, Image.LANCZOS)
         img = img.convert('RGB')
         img.save(file_path, quality=quality)
 
